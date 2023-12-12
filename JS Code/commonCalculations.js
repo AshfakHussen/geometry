ef659db7276ec3023e2e4a476d1b3dcbdee4c1a6
Ashfak Hussen <133750223+HussenAshfak@users.noreply.github.com>
2023-12-12 02:54:12 -0500

    first commit

---
+++ JS Code/commonCalculations.js
@@ -0,0 +1,95 @@
+///find input
+///common function
+function getTheInput(element) {
+    const infoHolder = document.getElementById(element);
+    const infoHolderString = infoHolder.value;
+    const infoInNumber = parseFloat(infoHolderString);
+    return infoInNumber;
+}
+
+///Common calling the from for display
+function formCalling(selectForm) {
+    const form = document.getElementById(selectForm);
+    form.classList.toggle('hidden');
+    return form;
+}
+///Triangle
+function calculateTheTriangleOutput() {
+    const triangle1 = getTheInput("triangleInputB");
+    const triangle2 = getTheInput("triangleInputH");
+    const tringleRegularValue = 0.5;
+    if (isNaN(triangle1) || isNaN(triangle2)) {
+        alert('Please Enter a Number');
+        return;
+    }
+    else {
+        const triangleResult = tringleRegularValue * triangle1 * triangle2;
+        return triangleResult;
+    }
+}
+
+function showTheTriangleOutput() {
+    const resultForm = document.getElementById('resultTriangle');
+    const result = parseFloat(calculateTheTriangleOutput().toFixed(2));
+    resultForm.innerText = result;
+    return result
+}
+
+///Rectangle
+function calculateRectangle() {
+    const inputW = getTheInput("rectangleInputW");
+    const inputI = getTheInput("rectangleInputI");
+    if (isNaN(inputI) || isNaN(inputW)) {
+        alert('Please Enter a Number!');
+        return;
+    } else {
+        const rectangleResult = inputI * inputW;
+        return rectangleResult;
+    }
+
+}
+
+function displayRectangle() {
+    const resultHolder = document.getElementById('resultRectangle');
+    const result=parseFloat(calculateRectangle().toFixed(2));
+    resultHolder.innerText = result;
+    return result;
+}
+
+///find innerText
+///common function
+function getInnerText(element){
+    const value=document.getElementById(element)
+    const valueString=value.innerText;
+    const valueParseFloat=parseFloat(valueString);
+    return valueParseFloat;
+}
+/// Parallelogram
+function calculateParallelogram(){
+    const value1=getInnerText('parallelogramB');
+    const value2=getInnerText('parallelogramH');
+    const area=parseFloat(value1*value2.toFixed(2));
+    const display=document.getElementById('resultParallelogram');
+    display.innerText=area;
+    return display;
+}
+///pentagon
+function calculatePentagon(){
+    const value1=getInnerText('PentagonP');
+    const value2=getInnerText('PentagonB');
+    const value3=0.5;
+    const area=parseFloat(value1*value2*value3.toFixed(2));
+    const display=document.getElementById('resultPentagon');
+    display.innerText=area;
+    return display;
+}
+///Rhombus
+function calculateRhombus(){
+    const value1=getInnerText('rhombusD1');
+    const value2=getInnerText('rhombusD2');
+    const value3=0.5;
+    const area=parseFloat(value1*value2*value3.toFixed(2));
+    const display=document.getElementById('resultRhombus');
+    display.innerText=area;
+    return display;
+}
---
+++ JS Code/functionForShapes.js
@@ -0,0 +1,20 @@
+document.getElementById('triangleBtn').addEventListener('click',function(){
+    showTheTriangleOutput();
+    formCalling('result-Form-Triangle');
+})
+document.getElementById('rectangleBtn').addEventListener('click',function(){
+    displayRectangle();
+    formCalling('result-Form-Rectangle');
+})
+document.getElementById('parallelogramBtn').addEventListener('click',function(){
+    calculateParallelogram();
+    formCalling('result-Form-Parallelogram');
+})
+document.getElementById('pentagonBtn').addEventListener('click',function(){
+    calculatePentagon();
+    formCalling('result-Form-Pentagon');
+})
+document.getElementById('rhombusBtn').addEventListener('click',function(){
+    calculateRhombus();
+    formCalling('result-Form-Rhombus');
+})
