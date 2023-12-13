55c8c8b4e2cd10457af857e11cd2defab17b9966
Ashfak Hussen <133750223+HussenAshfak@users.noreply.github.com>
2023-12-12 19:04:52 -0500

    new page is created and connected with blog butto

--- JS Code/functionForShapes.js
+++ JS Code/functionForShapes.js
@@ -1,24 +1,29 @@
-document.getElementById('triangleBtn').addEventListener('click',function(){
+document.getElementById('triangleBtn').addEventListener('click', function () {
     showTheTriangleOutput();
     formCalling('result-Form-Triangle');
 })
-document.getElementById('rectangleBtn').addEventListener('click',function(){
+document.getElementById('rectangleBtn').addEventListener('click', function () {
     displayRectangle();
     formCalling('result-Form-Rectangle');
 })
-document.getElementById('parallelogramBtn').addEventListener('click',function(){
+document.getElementById('parallelogramBtn').addEventListener('click', function () {
     calculateParallelogram();
     formCalling('result-Form-Parallelogram');
 })
-document.getElementById('pentagonBtn').addEventListener('click',function(){
+document.getElementById('pentagonBtn').addEventListener('click', function () {
     calculatePentagon();
     formCalling('result-Form-Pentagon');
 })
-document.getElementById('rhombusBtn').addEventListener('click',function(){
+document.getElementById('rhombusBtn').addEventListener('click', function () {
     calculateRhombus();
     formCalling('result-Form-Rhombus');
 })
-document.getElementById('ellipseBtn').addEventListener('click',function(){
+document.getElementById('ellipseBtn').addEventListener('click', function () {
     calculateEllipse();
-    formCalling("result-Form-Ellipse") ;
+    formCalling("result-Form-Ellipse");
+})
+///blog button
+document.getElementById('btnBlog').addEventListener('click', function () {
+    window.open('http://127.0.0.1:5500/blog.html', '_blank');
+
 })
